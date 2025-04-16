403d9f2fb7751dd4bc560ed9373974fe6d7a3000
Maksym <simbanyan98@gmail.com>
2025-04-16 19:28:27 +0200

    Fixes

--- databases/week1/homework.sql
+++ databases/week1/homework.sql
@@ -1,19 +1,17 @@
 --1.Find out how many tasks are in the task table
-SELECT COUNT(*) FROM task;
+SELECT COUNT(*) as total_tasks FROM task;
 --2.Find out how many tasks in the task table do not have a valid due date
-SELECT COUNT(*) FROM task WHERE due_date IS NULL;
+SELECT COUNT(*) as no_valid_date_tasks FROM task WHERE due_date IS NULL;
 --3.Find all the tasks that are marked as done
 SELECT title, status.name
 FROM task
 JOIN status ON task.status_id = status.id
-WHERE status.name = 'Done'
-GROUP BY task.title;
+WHERE status.name = 'Done';
 --4.Find all the tasks that are not marked as done
-SELECT COUNT(task.id) as total_tasks, task.title as task_title, status.name as status_name
+SELECT task.id, task.title, status.name
 FROM task
 JOIN status ON task.status_id = status.id
-WHERE status.name IN ('Not started', 'In progress')
-GROUP BY task.title, status.name;
+WHERE status.name <> "Done";
 --5.Get all the tasks, sorted with the most recently created first
 SELECT title, created FROM task
 ORDER BY created DESC;
@@ -25,16 +23,16 @@ LIMIT 1;
 SELECT title, due_date FROM task
 WHERE title LIKE '%database%' OR description LIKE '%database%';
 --8.Get the title and status (as text) of all tasks
-SELECT title, status.name FROM task
+SELECT title, status.name AS status FROM task
 JOIN status ON task.status_id = status.id;
 --9.Get the name of each status, along with a count of how many tasks have that status
 SELECT status.name, COUNT(task.id) as total_tasks
 FROM task
-JOIN status ON task.status_id = status.id
+LEFT JOIN status ON task.status_id = status.id
 GROUP BY status.name;
 --10.Get the names of all statuses, sorted by the status with most tasks first
-SELECT status.name, COUNT(task.id) as total_tasks
+SELECT status.name as status, COUNT(task.id) as total_tasks
 FROM task
-JOIN status ON task.status_id = status.id
+LEFT JOIN status ON task.status_id = status.id
 GROUP BY status.name
-ORDER BY total_tasks DESC
+ORDER BY total_tasks DESC;
